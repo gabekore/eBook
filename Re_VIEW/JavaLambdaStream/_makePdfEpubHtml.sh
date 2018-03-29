4a427b80c6d91ce6d12f931abcb061019df3e06e
gabekore <boottechprogramming@gmail.com>
2018-03-30 07:20:05 +0900

    pdf、epub、htmlをbinディレクトリに出力するように変更

--- Re_VIEW/JavaLambdaStream/_makePdfEpubHtml.sh
+++ Re_VIEW/JavaLambdaStream/_makePdfEpubHtml.sh
@@ -15,6 +15,12 @@ ruby -v
 echo "Press the Enter Key..."
 read Wait
 
+echo "binフォルダ作成"
+mkdir bin
+# ↓pause代わり、Waitは変数になる
+echo "Press the Enter Key..."
+read Wait
+
 
 # PDFの作成
 echo "◆PDFの作成を開始します"
@@ -22,8 +28,7 @@ echo "◆PDFの作成を開始します"
 echo "Press the Enter Key..."
 read Wait
 review-pdfmaker config.yml
-
-
+mv *.pdf ./bin/.
 
 
 # EPUBの作成
@@ -32,6 +37,7 @@ echo "◆EPUBの作成を開始します"
 echo "Press the Enter Key..."
 read Wait
 review-epubmaker config.yml
+mv *.epub ./bin/.
 
 
 
@@ -40,14 +46,16 @@ echo "◆HTMLの作成を開始します"
 # ↓pause代わり、Waitは変数になる
 echo "Press the Enter Key..."
 read Wait
-mkdir html
-review-compile --target html ch01.re > ./html/ch01.html 
-review-compile --target html ch02.re > ./html/ch02.html
-review-compile --target html ch03.re > ./html/ch03.html
-review-compile --target html ch04.re > ./html/ch04.html
-review-compile --target html ch05.re > ./html/ch05.html
-review-compile --target html ch06.re > ./html/ch06.html
-review-compile --target html ch07.re > ./html/ch07.html
-review-compile --target html ch08.re > ./html/ch08.html
+mkdir ./bin/html
+review-compile --target html ch01.re > ./bin/html/ch01.html 
+review-compile --target html ch02.re > ./bin/html/ch02.html
+review-compile --target html ch03.re > ./bin/html/ch03.html
+review-compile --target html ch04.re > ./bin/html/ch04.html
+review-compile --target html ch05.re > ./bin/html/ch05.html
+review-compile --target html ch06.re > ./bin/html/ch06.html
+review-compile --target html ch07.re > ./bin/html/ch07.html
+review-compile --target html ch08.re > ./bin/html/ch08.html
+review-compile --target html ch09.re > ./bin/html/ch09.html
+review-compile --target html chA.re  > ./bin/html/chA.html
 
 
